ced0fdebaf5363c18a01c9422560498c64925aa8
Wren <vetens@wisc.edu>
2019-11-01 14:09:17 +0100

    quick fix to condor template submission script

--- condor/condor_template.sh
+++ condor/condor_template.sh
@@ -3,5 +3,4 @@ cd WORKDIR
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`
 #export X509_USER_PROXY=userproxy
-export PYTHONPATH=$PYTHONPATH:/cvmfs/cms.cern.ch/slc6_amd64_gcc700/external/python/2.7.14-omkpbe4/bin/python
 ./run.py -x ISDAT -l LISTDIR/listLISTNUMBER -o TARGETRUNTYPECondorJob_JOBNUMBER.root
